1ca5cf37de088f93a4f060dc5d8fdf2235113a54
Pratik Goel <pratik.goel@rakuten.com>
2022-11-20 18:46:13 +0530

    Updated project dependencies

---
+++ GoogleCalendarAPIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleCalendarAPIDemo
+//
+//  Created by Goel, Pratik | RIEPL on 20/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoogleCalendarAPIDemo/GoogleCalendarAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleCalendarAPIDemoApp.swift
+//  GoogleCalendarAPIDemo
+//
+//  Created by Goel, Pratik on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleCalendarAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
